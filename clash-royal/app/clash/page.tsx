8ecfb38e4d1190d6d0aec2ac183e172f447b981d
AilesVax <raphael.vaxelaire10@gmail.com>
2026-03-03 11:17:55 +0100

    call api

---
+++ clash-royal/app/clash/page.tsx
@@ -0,0 +1,22 @@
+import { fetchCards, Card } from "@/serv/lien"; // adapte le chemin
+
+export default async function Home() {
+  const cards: Card[] = await fetchCards();
+
+  return (
+    <main>
+      <h1>Cartes Clash Royale</h1>
+
+      <div style={{ display: "grid", gridTemplateColumns: "repeat(4, 1fr)", gap: "20px" }}>
+        {cards.map((card) => (
+          <div key={card.id} style={{ border: "1px solid gray", padding: "10px" }}>
+            <h3>{card.name}</h3>
+            <p>Rareté : {card.rarity}</p>
+            <p>Elixir : {card.elixirCost}</p>
+            <p>Max Level : {card.maxLevel}</p>
+          </div>
+        ))}
+      </div>
+    </main>
+  );
+}
---
+++ clash-royal/serv/lien.tsx
@@ -0,0 +1,37 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const API_KEY = process.env.CLASH_API_KEY;
+
+if (!API_KEY) {
+  throw new Error("CLASH_API_KEY non définie dans le .env");
+}
+export interface Card {
+  name: string;
+  id: number;
+  maxLevel: number;
+  maxEvolutionLevel: number;
+  elixirCost: number;
+  rarity: string;
+}
+
+export async function fetchCards(): Promise<Card[]> {
+  try {
+    const response = await fetch('https://api.clashroyale.com/v1/cards', {
+      headers: {
+        Authorization: `Bearer ${API_KEY}`, 
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Erreur HTTP: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data.items as Card[]; 
+  } catch (error) {
+    console.error('Erreur lors de l’appel API :', error);
+    return [];
+  }
+}
