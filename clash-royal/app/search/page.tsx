f3e30d3b772e5563866ede7c143a7133e70960d1
max <maximilien.ilic@gmail.com>
2026-03-06 12:06:32 +0100

    add:css recherche

--- clash-royal/app/search/page.tsx
+++ clash-royal/app/search/page.tsx
@@ -1,8 +1,10 @@
-import Recherche from "@/components/Recherche"
+import Recherche from "@/components/Recherche/Recherche"
+import style from './search.module.css';
 
 export default function Home() {
   return (
-    <main>
+    <main className={style.main}>
+      <h1 className={style.title}>Rechercher profil:</h1>
       <Recherche />
     </main>
   )
--- clash-royal/components/Recherche/Recherche.tsx
+++ clash-royal/components/Recherche/Recherche.tsx
@@ -3,29 +3,29 @@
 import { Player } from "@/serv/lien"
 import { useFormState } from "react-dom"
 import { searchPlayer } from "@/actions/player"
+import styles from "./Recherche.module.css"
 
 export default function Recherche() {
   const [state, action] = useFormState<Player | null, FormData>(searchPlayer, null) 
 
   return (  
     <div>  
-      <form action={action}>  
-        <input type="text" name="tag" />
-        <button type="submit">Rechercher</button>
+      <form action={action} className={styles.form}>
+        <input className={styles.input} type="text" name="tag" placeholder="#TAG du joueur" />
+        <button className={styles.bouton} type="submit">Rechercher</button>
       </form>
-
-      {state && (  
-        <div style={{ border: "1px solid gray", padding: "10px" }}>
-          <h3>{state.name}</h3>  
+      {state && (
+        <div className={styles.joueur}>
+          <h3 >{state.name}</h3>
           <p>Clan : {state.clan?.name}</p>
           <p>Arena : {state.arena?.name}</p>
           <p>Wins : {state.wins}</p>
           <p>Losses : {state.losses}</p>
           <p>Carte favorite : {state.currentFavouriteCard?.name}</p>
-          <div className="grid grid-cols-4 gap-2 p-4 rounded-2xl w-fit">
+          <div className={styles.deck}>
             {state.currentDeck.map((card) => (
-              <div key={card.id} className="relative w-20 h-24 rounded-xl overflow-hidden border-2 border-purple-600 shadow-lg hover:scale-105 transition-transform duration-200">
-                <img src={card.iconUrls.medium} alt={card.name} className="w-full h-full object-cover" />
+              <div key={card.id} className={styles.carte}>
+                <img src={card.iconUrls.medium} alt={card.name} />
               </div>
             ))}
           </div>
