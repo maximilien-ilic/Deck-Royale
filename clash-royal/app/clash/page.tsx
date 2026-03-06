d13d85ac83046a51b0e35ba9869d77c666bedd32
carls-eth <charles.bauchet@edu.devinci.fr>
2026-03-06 09:33:57 +0100

    ajout du css et de la barre de recherche

--- clash-royal/app/clash/page.tsx
+++ clash-royal/app/clash/page.tsx
@@ -1,26 +1,14 @@
+import style from './clash.module.css';
 import { fetchCards, Card } from "@/serv/api"; 
+import SearchBar from "@/components/Searchbar/Searchbar";
 
 export default async function Home() {
-  const cards: Card[] = await fetchCards();
+const cards: Card[] = await fetchCards();
 
   return (
-    <main>
-      <h1>Cartes Clash Royale</h1>
-
-      <div style={{ display: "grid", gridTemplateColumns: "repeat(4, 1fr)", gap: "20px" }}>
-        {cards.map((card) => (
-          <div key={card.id} style={{ border: "1px solid gray", padding: "10px" }}>
-            <h3>{card.name}</h3>
-            <p>Rareté : {card.rarity}</p>
-            <p>Elixir : {card.elixirCost}</p>
-            <img
-            src={card.iconUrls.medium}
-            alt={card.name}
-            width={100}
-            />
-          </div>
-        ))}
-      </div>
+    <main className={style.main}>
+      <h1 className={style.titre}>DECK ROYAL</h1>
+      <SearchBar cards={cards}/>
     </main>
   );
 }
--- clash-royal/app/random/page.tsx
+++ clash-royal/app/random/page.tsx
@@ -1,23 +1,27 @@
 
 
+import SearchBar from "@/components/Searchbar/Searchbar";
 import { fetchCards, Card } from "@/serv/api";
 import { getRandomCards } from "@/serv/random";
-
+import style from './random.module.css';
 export default async function Home() {
   const cards: Card[] = await fetchCards();
 
   const randomCards = getRandomCards(cards, 8);
 
   return (
-    <main>
-      <h1>Deck aléatoire</h1>
-        <a href="./random">relancer</a>
+    <main className={style.main}>
+      <h1 className={style.titre}>Deck aléatoire</h1>
+      <p className={style.paragraph}>Générez un deck aléatoire en cliquant sur le bouton ci-dessous.</p>
+              <a href="./random" className={style.bouton}>Générer un nouveau deck</a>
+      <div className={style.cardcontainer}>
       {randomCards.map((card) => (
-        <div key={card.id}>
+        <div className={style.card} key={card.id}>
           <img src={card.iconUrls.medium} width={100} />
           <p>{card.name}</p>
         </div>
       ))}
+      </div>
     </main>
   );}
   
